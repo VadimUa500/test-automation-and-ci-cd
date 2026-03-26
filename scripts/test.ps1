b93a72470b86e6c4dfc4f4f3ecd25bb0c458148a
VadimUa500 <vadimua578@gmail.com>
2026-03-26 14:28:53 +0200

    4

--- scripts/test.ps1
+++ scripts/test.ps1
@@ -2,7 +2,10 @@ $ErrorActionPreference = "Stop"
 
 New-Item -ItemType Directory -Force -Path reports | Out-Null
 
-pytest sample_py > reports/test_output.txt 2>&1
+cd sample_py
+pytest test_math.py > ../reports/test_output.txt 2>&1
+
+cd ..
 
 Get-Content reports/test_output.txt
 
